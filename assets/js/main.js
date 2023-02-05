42b2d74b6678799f09739cef31a7d1065c33096a
Adam Eljaafari <70715586+Osteopourosis@users.noreply.github.com>
2023-02-05 16:06:46 +0000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,9 @@
-/*
-	Full Motion by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
 
 (function($) {
 
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
+		large:	'(max-width: 1200px)',
 		medium:	'(max-width: 980px)',
 		small:	'(max-width: 736px)',
 		xsmall:	'(max-width: 480px)'
